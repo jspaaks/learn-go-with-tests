e17d1a8f79c08e0846ed8c1071972e37da93d3ce
Jurriaan H. Spaaks <jhspaaks@hotmail.com>
2025-04-29 11:46:03 -0400

    added SumAll and SumAllTails functions and their tests

--- arrays/pkg/arrays/arrays.go
+++ arrays/pkg/arrays/arrays.go
@@ -1,8 +1,29 @@
 package arrays
 
 func Sum(numbers []int) (summed int) {
+	if len(numbers) == 0 {
+		return
+	}
 	for _, number := range numbers {
 		summed += number
+	}
+	return
+}
+
+func SumAll(slices ...[]int) (summed []int) {
+	for _, slice := range slices {
+		summed = append(summed, Sum(slice))
+	}
+	return
+}
+
+func SumAllTails(slices ...[]int) (summed []int) {
+	for _, slice := range slices {
+		if len(slice) == 0 {
+			summed = append(summed, 0)
+		} else {
+			summed = append(summed, Sum(slice[1:]))
+		}
 	}
 	return
 }
--- arrays/pkg/arrays/arrays_test.go
+++ arrays/pkg/arrays/arrays_test.go
@@ -1,14 +1,105 @@
 package arrays
 
-import "testing"
+import (
+	"testing"
+	"slices"
+)
 
 func TestSum(t *testing.T) {
-	t.Run("Slice of length 3", func (t *testing.T){
+
+	assertEqual := func(t *testing.T, got int, want int, input []int) {
+		t.Helper()
+		if got != want {
+			t.Errorf("Got %d, but want %d given %v", got, want, input)
+		}
+	}
+
+	t.Run("empty slice", func (t *testing.T){
+		var numbers = []int{}
+		var got int = Sum(numbers)
+		var want int = 0
+		assertEqual(t, got, want, numbers)
+	})
+
+	t.Run("slice of length 3", func (t *testing.T){
 		var numbers = []int{1, 2, 3}
 		var got int = Sum(numbers)
 		var want int = 6
-		if got != want {
-			t.Errorf("Got %d, but want %d given %v", got, want, numbers)
+		assertEqual(t, got, want, numbers)
+	})
+}
+
+func TestSumAll(t *testing.T) {
+
+	assertEqual := func(t *testing.T, got []int, want []int, input [][]int) {
+		t.Helper()
+		if !slices.Equal(got, want) {
+			t.Errorf("Got %d, but want %d given %v", got, want, input)
 		}
+	}
+
+	t.Run("one slice", func(t *testing.T) {
+		var a = []int{1, 1, 1}
+		var input = [][]int{a}
+		got := SumAll(input...)
+		want := []int{3}
+		assertEqual(t, got, want, input)
+	})
+
+	t.Run("two slices", func(t *testing.T) {
+		var a = []int{1, 2}
+		var b = []int{0, 9}
+		var input = [][]int{a, b}
+		got := SumAll(input...)
+		want := []int{3, 9}
+		assertEqual(t, got, want, input)
+	})
+
+}
+
+func TestSumAllTails(t *testing.T) {
+
+	assertEqual := func(t *testing.T, got []int, want []int, input [][]int) {
+		t.Helper()
+		if !slices.Equal(got, want) {
+			t.Errorf("Got %d, but want %d given %v", got, want, input)
+		}
+	}
+
+	t.Run("one slice", func(t *testing.T) {
+		var a = []int{1, 1, 1}
+		var input = [][]int{a}
+		got := SumAllTails(input...)
+		want := []int{2}
+		assertEqual(t, got, want, input)
+	})
+
+	t.Run("two slices", func(t *testing.T) {
+		var a = []int{1, 2}
+		var b = []int{0, 9}
+		var input = [][]int{a, b}
+		got := SumAllTails(input...)
+		want := []int{2, 9}
+		assertEqual(t, got, want, input)
+	})
+
+	t.Run("three short slices", func(t *testing.T) {
+		var a = []int{1}
+		var b = []int{11}
+		var c = []int{111}
+		var input = [][]int{a, b, c}
+		got := SumAllTails(input...)
+		want := []int{0, 0, 0}
+		assertEqual(t, got, want, input)
+	})
+
+	t.Run("three empty slices", func(t *testing.T) {
+		var a = []int{}
+		var b = []int{}
+		var c = []int{}
+		var input = [][]int{a, b, c}
+		got := SumAllTails(input...)
+		want := []int{0, 0, 0}
+		assertEqual(t, got, want, input)
 	})
 }
