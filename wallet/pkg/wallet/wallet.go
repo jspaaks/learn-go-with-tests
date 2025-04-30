a58ec8313b38672fd7a3939fec080de5cd1ecffa
Jurriaan H. Spaaks <jhspaaks@hotmail.com>
2025-04-30 16:11:50 -0400

    moved the wallet package errors to its own file

--- wallet/pkg/wallet/wallet.go
+++ wallet/pkg/wallet/wallet.go
@@ -1,16 +1,5 @@
 package wallet
 
-import "errors"
-
-// Error issued when a user tries to withdraw more money from the wallet than they have available.
-var InsufficientFundsError = errors.New("Insufficient funds available")
-
-// Error issued when the user tries to withdraw a negative amount.
-var NegativeWithdrawalError = errors.New("Can't withdraw a negative number")
-
-// Error issued when the user tries to deposit a negative amount.
-var NegativeDepositError = errors.New("Can't deposit a negative number")
-
 // Defintion of a wallet.
 type Wallet struct {
 	balance Bitcoin
