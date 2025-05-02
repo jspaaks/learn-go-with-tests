b5bef3af8c2b7f9724cf2c72688f05518603d2c9
Jurriaan H. Spaaks <jhspaaks@hotmail.com>
2025-05-02 14:52:58 -0400

    added sleeping in between counting; test is slow now

--- mocking/pkg/countdown/countdown.go
+++ mocking/pkg/countdown/countdown.go
@@ -3,12 +3,14 @@ package countdown
 import (
 	"fmt"
 	"io"
+	"time"
 )
 
 func Countdown(writer io.Writer) {
 	const startFrom int = 3
 	for i := startFrom; i > 0; i-- {
 		fmt.Fprintf(writer, "%d\n", i)
+		time.Sleep(1 * time.Second)
 	}
 	fmt.Fprint(writer, "Go!\n")
 }
