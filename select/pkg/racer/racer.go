d088e42a59f860eb586bc549a9083eb3cf200600
Jurriaan H. Spaaks <jhspaaks@hotmail.com>
2025-05-05 16:16:04 -0400

    added timeout to racer

--- select/pkg/racer/racer.go
+++ select/pkg/racer/racer.go
@@ -1,8 +1,12 @@
 package racer
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
 
-func Racer(url0 string, url1 string) (winner string) {
+func Racer(url0 string, url1 string, timeout time.Duration) (winner string, err error) {
 	var ping = func(url string) chan struct{} {
 		ch := make(chan struct{})
 		go func (){
@@ -14,8 +18,10 @@ func Racer(url0 string, url1 string) (winner string) {
 
 	select {
 	case <-ping(url0):
-		return url0
+		return url0, nil
 	case <-ping(url1):
-		return url1
+		return url1, nil
+	case <-time.After(timeout):
+		return "", fmt.Errorf("Timed out waiting for %q and %q", url0, url1)
 	}
 }
--- select/pkg/racer/racer_test.go
+++ select/pkg/racer/racer_test.go
@@ -7,16 +7,18 @@ import (
 	"time"
 )
 
+var getServerAndUrl = func(delay time.Duration) (server *httptest.Server, url string){
+	delayedHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(delay)
+		w.WriteHeader(http.StatusOK)
+	})
+	server = httptest.NewServer(delayedHandler)
+	url = server.URL
+	return
+}
+
+
 func TestRacer(t *testing.T) {
-	getServerAndUrl := func(delay time.Duration) (server *httptest.Server, url string){
-		delayedHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			time.Sleep(delay)
-			w.WriteHeader(http.StatusOK)
-		})
-		server = httptest.NewServer(delayedHandler)
-		url = server.URL
-		return
-	}
 
 	fastServer, fastUrl := getServerAndUrl(2 * time.Millisecond)
 	defer fastServer.Close()
@@ -24,9 +26,25 @@ func TestRacer(t *testing.T) {
 	slowServer, slowUrl := getServerAndUrl(20 * time.Millisecond)
 	defer slowServer.Close()
 
-	want := fastUrl
-	got := Racer(fastUrl, slowUrl)
-	if got != want {
-		t.Errorf("Wanted %q but got %q instead", want, got)
-	}
+	t.Run("selecting the url whose server responds fastest", func(t *testing.T){
+		want := fastUrl
+		got, _ := Racer(fastUrl, slowUrl, 10 * time.Second)
+		if got != want {
+			t.Errorf("Wanted %q but got %q instead", want, got)
+		}
+	})
+
+	t.Run("timing out after if neither server has responded after 10 seconds", func(t *testing.T){
+		fastServer, fastUrl := getServerAndUrl(11 * time.Second)
+		defer fastServer.Close()
+
+		slowServer, slowUrl := getServerAndUrl(12 * time.Second)
+		defer slowServer.Close()
+
+		_, err := Racer(fastUrl, slowUrl, 10 * time.Second)
+		if err == nil {
+			t.Errorf("Expected an error but didn't get one")
+		}
+	})
+
 }
