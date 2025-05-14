67b2665d852c83da077357d5cc4342b24a147257
Jurriaan H. Spaaks <jhspaaks@hotmail.com>
2025-05-14 10:37:28 -0400

    refactored test into an approval test using external dependency; added snapshotted expected result

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/jspaaks/learn-go-with-tests
 
 go 1.22.2
+
+require github.com/approvals/go-approval-tests v1.5.0 // indirect
--- templating/pkg/blogrenderer/blogrenderer_test.go
+++ templating/pkg/blogrenderer/blogrenderer_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"github.com/jspaaks/learn-go-with-tests/templating/pkg/blogposts"
 	"github.com/jspaaks/learn-go-with-tests/templating/pkg/blogrenderer"
+	"github.com/approvals/go-approval-tests"
 	"testing"
 )
 
@@ -11,12 +12,6 @@ func assertNoErrors(t *testing.T, err error) {
 	t.Helper()
 	if err != nil {
 		t.Fatalf("Didn't expect an error but got one: %s", err)
-	}
-}
-func assertCorrectHtml(t *testing.T, actual string, expected string) {
-	t.Helper()
-	if actual != expected {
-		t.Fatalf("actual HTML string %q didn't match expected HTML string %q", actual, expected)
 	}
 }
 
@@ -30,16 +25,7 @@ func TestBlogRenderer(t *testing.T) {
 		}
 		buf := bytes.Buffer{}
 		err := blogrenderer.Render(&buf, post)
-		expected := 
-`<h1>This is the post title</h1>
-<p>This is the post description</p>
-Tags:
-<ul>
-    <li>go</li>
-    <li>tdd</li>
-</ul>
-`
 		assertNoErrors(t, err)
-		assertCorrectHtml(t, buf.String(), expected)
+		approvals.VerifyString(t, buf.String())
 	})
 }
