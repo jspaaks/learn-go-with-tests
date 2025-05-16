e31aeb6d0613b4833b16e49236e0dda720cc5812
Jurriaan H. Spaaks <jhspaaks@hotmail.com>
2025-05-16 10:35:31 -0400

    interface for racer.Racer() has changed, updated call

--- select/cmd/racer/racer-cli.go
+++ select/cmd/racer/racer-cli.go
@@ -3,13 +3,12 @@ package main
 import (
     "fmt"
     "github.com/jspaaks/learn-go-with-tests/select/pkg/racer"
-    "time"
 )
 
 func main() {
     url0 := "https://google.com"
     url1 := "https://fairsoftwarechecklist.net/v0.2"
-    fastest, err := racer.Racer(url0, url1, 3 * time.Second)
+    fastest, err := racer.Racer(url0, url1)
     if err != nil {
         fmt.Println(err)
     }
